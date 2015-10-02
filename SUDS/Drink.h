75cef525f82d4cba97e54a91e455f91bb40770e4
Mitchell Au <aumitche@gmail.com>
2015-10-02 17:34:48 -0400

    Backing up before updating build settings

---
+++ SUDS/Drink.h
@@ -0,0 +1,17 @@
+//
+//  Drink.h
+//  
+//
+//  Created by aumitche on 2015–09–12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Drink : NSObject
+
+//Declare the member variables
+NSString type;
+double price;
+
+@end
---
+++ SUDS/Drink.m
@@ -0,0 +1,13 @@
+//
+//  Drink.m
+//  
+//
+//  Created by aumitche on 2015–09–12.
+//
+//
+
+#import "Drink.h"
+
+@implementation Drink
+
+@end
